c14875ca3307dd2d64ec183b6c37aea22d09abb2
Jon Sweeney <jonsweeney26@gmail.com>
2021-06-11 17:37:51 -0700

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Jonathan Sweeney on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
